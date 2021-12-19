7fc2a9076abe4015aff307d7a33756b0633fb146
khalil <khalilpati55@gmail.com>
2021-12-19 12:46:38 +0530

    UI Changes

--- static/js/script.js
+++ static/js/script.js
@@ -1,14 +1,16 @@
-const btnCloseModal = document.querySelector(".close-modal");
+const btnCloseModal = document.querySelectorAll(".close-modal");
 const modalTransaction = document.querySelector(".transaction-modal");
 const modalCategory = document.querySelector(".category-modal");
 const overlay = document.querySelector(".overlay");
 
 const openModalTransaction = function () {
+  window.scrollTo(0,0);
   modalTransaction.classList.remove("hidden");
   overlay.classList.remove("hidden");
 };
 
 const openModalCategory = function () {
+  window.scrollTo(0,0);
   modalCategory.classList.remove("hidden");
   overlay.classList.remove("hidden");
 };
@@ -17,9 +19,13 @@ const closeModal = function () {
   modalCategory.classList.add("hidden");
   modalTransaction.classList.add("hidden");
   overlay.classList.add("hidden");
+  document.body.style.overflow = 'visible';
 };
 
-btnCloseModal.addEventListener("click", closeModal);
+btnCloseModal.forEach((btn)=>{
+  btn.addEventListener("click", closeModal);
+})
+
 overlay.addEventListener("click", closeModal);
 
 const openTransactionModal = () => openModalTransaction();
