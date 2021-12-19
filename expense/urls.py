c33796116cced3c8285850fb4f0ee7fbdb0829b3
khalil <khalilpati55@gmail.com>
2021-12-19 18:52:09 +0530

    Made some Changes

--- expense/urls.py
+++ expense/urls.py
@@ -23,9 +23,4 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', include('base.urls')),
-]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-
-urlpatterns+= staticfiles_urlpatterns()
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
